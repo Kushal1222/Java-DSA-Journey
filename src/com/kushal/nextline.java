6e9f35fe6d97fe99005bc18bfb81ec6a098a21a7
Kushal <k.jawrani04@gmail.com>
2026-05-03 00:08:22 +0530

    using NextLine

---
+++ src/com/kushal/nextline.java
@@ -0,0 +1,15 @@
+package com.kushal;
+import java.util.*;
+
+
+public class nextline {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String line=sc.nextLine();
+        //nextInt()
+        //nextFloat()
+
+        System.out.println(line);
+
+    }
+}
