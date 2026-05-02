69597b59017830fe6d8cf721e5fc33fa289bbc27
Kushal <k.jawrani04@gmail.com>
2026-05-03 00:08:47 +0530

    multiplying 2 numbers

---
+++ src/com/kushal/multiply.java
@@ -0,0 +1,10 @@
+package com.kushal;
+
+public class multiply {
+    public static void main(String[] args){
+        int a=20;
+        int b=10;
+        int mult=a*b;
+        System.out.println(mult);
+    }
+}
