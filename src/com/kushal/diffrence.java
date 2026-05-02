f88a3ecd5d11973ef5228fa527339cbf5bbe47d9
Kushal <k.jawrani04@gmail.com>
2026-05-02 23:48:13 +0530

    Day1-Hello world

---
+++ src/com/kushal/diffrence.java
@@ -0,0 +1,10 @@
+package com.kushal;
+
+public class diffrence {
+    public static void main(String[] args) {
+        int a =10;
+        int b=20;
+        int difference=b-a;
+        System.out.println(difference);
+    }
+}
---
+++ src/com/kushal/input.java
@@ -0,0 +1,11 @@
+package com.kushal;
+import java.util.*;
+
+public class input {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String name=sc.next();
+        System.out.println(name);
+
+    }
+}
