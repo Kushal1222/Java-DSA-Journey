d842340345262e1a4ce8fb424534b46ac4621de2
Kushal <k.jawrani04@gmail.com>
2026-05-03 00:07:40 +0530

    Variables in Java

---
+++ src/com/kushal/variables.java
@@ -0,0 +1,15 @@
+package com.kushal;
+
+public class variables {
+    public static void main(String[] args){
+        String name="Tony stark";
+        int a=25;
+        int b=10;
+        double price=25.25;
+        name="kushal";
+        System.out.println(name);
+        System.out.println(a+b);
+        System.out.println(price+a);
+
+    }
+}
