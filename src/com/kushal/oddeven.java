c58aab11e11114eaa47e2520124d73728e4323ce
Kushal1222 <k.jawrani04@gmail.com>
2026-05-06 00:02:32 +0530

    To print Whether the number is odd /even

---
+++ src/com/kushal/oddeven.java
@@ -0,0 +1,16 @@
+package com.kushal;
+import java.util.*;
+
+public class oddeven {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        if(x%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("odd");
+        }
+
+    }
+}
