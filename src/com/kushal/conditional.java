7fd56da7361bec98e15e995344c83b73468fbf69
Kushal1222 <k.jawrani04@gmail.com>
2026-05-05 23:59:38 +0530

    Add files via upload
    Conditional Statements in Java

---
+++ src/com/kushal/conditional.java
@@ -0,0 +1,17 @@
+package com.kushal;
+import java.util.*;
+
+public class conditional {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int age=sc.nextInt();
+        if(age>=18) {
+            System.out.println("Adult");
+
+        }
+        else {
+            System.out.println("not adult");
+        }
+
+    }
+}
